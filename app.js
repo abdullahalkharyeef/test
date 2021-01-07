af82a299dd69d8527e4bfdec16a26092686f4bec
alkharyeef <40389935+abdullahalkharyeef@users.noreply.github.com>
2021-01-07 08:51:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -506,8 +506,6 @@ setTimeout(play1,5000);
 }
 
 function play1 () {
-    var elementFirst = document.getElementsByClassName("animattop");
-    elementFirst.classList.add("w3-container\sw3-centerw3-animate-to");
     document.getElementById("svg_front").style.display = "none";
     myApp1.card1= true;
     myApp.title= true;
